5e684d958c2302292242337d02203be997a2ad10
Nicolas Pernoud <nper.public@gmail.com>
2018-04-15 14:03:46 +0200

    Switched  build to x64

--- server/src/server.ts
+++ server/src/server.ts
@@ -15,7 +15,7 @@ const server = app.listen(app.get('port'), () => {
  */
 const securePort = process.env.SECURE_PORT ? process.env.SECURE_PORT : 443;
 const certPath = process.env.SSL_CERT_LOCATION ? process.env.SSL_CERT_LOCATION + '/fullchain.pem' : './certificates/localhost.crt';
-const keyPath = process.env.SSL_CERT_LOCATION ? process.env.SSL_CERT_LOCATION + '/privkey.pem' : './certificates/localhost.key';
+const keyPath = process.env.SSL_CERT_LOCATION ? process.env.SSL_CERT_LOCATION + '/private/privkey.pem' : './certificates/localhost.key';
 const options = {
   cert: fs.readFileSync(certPath),
   key: fs.readFileSync(keyPath)
